8ebeadfeb891e225082804db17b885bfd563c1ed
Raúl <raulacostadeveloper@outlook.com>
2022-05-19 19:20:23 -0500

    imagen y sonidos

---
+++ js/sonidos.js
@@ -0,0 +1,10 @@
+const pop = new Audio('./Audios/pop.mp3');
+const click = new Audio('./Audios/click.mp3');
+
+
+function reproducirPop(){
+    pop.play();
+}
+function reproducirClick(){
+    click.play();
+}
